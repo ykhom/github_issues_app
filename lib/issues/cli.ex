cc08ef4e2e4fb98984680fcd87e20de5f0981a40
Yurii Khomitskyi <yurii.khomitskyi@outlook.com>
2019-01-07 15:32:09 +0200

    Added Parses and HTTPoison

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -6,7 +6,9 @@ defmodule Issues.CLI do
   table of the last _n_ issues in a github project
   """
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
   @doc """
   `argv` can be -h or --help, which returns :help.
@@ -14,14 +16,47 @@ defmodule Issues.CLI do
   the number of entries to format.
   Return a tuple of `{ user, project, count }`, or `:help` if help was given.
   """
+
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [ help: :boolean],
-    aliases: [ h: :help ])
-    case parse do
-      { [ help: true ], _,           _ } -> :help
-      { _, [ user, project, count ], _ } -> { user, project, String.to_integer(count) }
-      { _, [ user, project ],        _ } -> { user, project, @default_count }
-      _                                  -> :help
-    end
+    OptionParser.parse(argv, switches: [ help: :boolean],
+                                            aliases:  [ h:    :help    ])
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
+  def args_to_internal_representation([user, project, count]) do
+    { user, project, String.to_integer(count) }
+  end
+  def args_to_internal_representation([user, project]) do
+    { user, project, @default_count }
+  end
+  def args_to_internal_representation(_) do # bad arg or --help
+    :help
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+     |> decode_response()
+     |> sort_into_descending_order()
+  end
+
+  def decode_response({:ok, body}), do: body
+
+  def decode_response({:error, error}) do
+    IO.puts "Error fetching from Github: #{error["message"]}"
+    System.halt(2)
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 ->
+        i1["created_at"] >= i2["created_at"]
+      end)
   end
 end
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -1,8 +1,9 @@
 defmodule CliTest do
   use ExUnit.Case
+  import Issues.CLI, only: [ parse_args: 1,
+                             sort_into_descending_order: 1 ]
   doctest Issues
 
-  import Issues.CLI, only: [parse_args: 1]
   test ":help returned by option parsing with -h and --help options" do
     assert parse_args(["-h", "anything"]) == :help
     assert parse_args(["--help", "anything"]) == :help
@@ -14,5 +15,16 @@ defmodule CliTest do
 
   test "count is defaulted if two values given" do
     assert parse_args(["user", "project"]) == { "user", "project", 4}
+  end
+
+  test "sort descending orders the correct way" do
+    result = sort_into_descending_order(fake_created_at_list(["c", "a", "b"]))
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    assert issues == ~w{ c b a }
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values,
+    do: %{"created_at" => value, "other_data" => "xxx"}
   end
 end
